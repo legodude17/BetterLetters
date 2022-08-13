4032b26941f336736583e63a58c91ffad57518e0
Joshua <legodude17@users.noreply.github.com>
2022-08-12 19:15:46 -0700

    Split versions

---
+++ 1.3/Source/BetterLetters/LetterUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Verse;
+
+namespace BetterLetters
+{
+    public static class LetterUtilities
+    {
+        public static void ClearAll(this LetterStack stack)
+        {
+            var letters = stack.LettersListForReading.ListFullCopy();
+            letters.ForEach(stack.RemoveLetter);
+        }
+
+        public static void ClearAll(this LetterStack stack, Predicate<Letter> shouldRemove)
+        {
+            var letters = stack.LettersListForReading.ListFullCopy();
+            letters.ForEach(letter =>
+            {
+                if (shouldRemove(letter)) stack.RemoveLetter(letter);
+            });
+        }
+    }
+}
